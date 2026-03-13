e1887ca79ed6d42597c046efc890c3c43398001a
Ubuntu <ubuntu@ip-172-31-40-0.ap-south-1.compute.internal>
2026-03-13 12:17:59 +0000

    changed the app.py file

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
 from flask import Flask
+
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def home():
-    return "This is Sunil who is working hard on Devops /n Loves Gayathri"
+    return """
+    <h2>This is Sunil who is working hard on DevOps</h2>
+    <h3>Loves Gayathri</h3>
+    """
 
-app.run(host='0.0.0.0',port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
+
+
